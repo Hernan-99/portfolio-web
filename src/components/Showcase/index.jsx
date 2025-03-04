2d4b7a9291e7f7e55f562c022809890894a74444
Hernan-99 <hernan24744@gmail.com>
2025-03-04 15:20:57 -0300

    importacion y creacion para el componente de proyectos y blog

---
+++ src/components/Showcase/index.jsx
@@ -0,0 +1,5 @@
+const Showcase = ({ title }) => {
+  return <div>{title}</div>;
+};
+
+export default Showcase;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
 import Jumbotron from "../components/Jumbotron";
-Jumbotron;
+import Showcase from "../components/Showcase";
 export const Main = () => {
   return (
     <main>
       <Hero />
+      <Showcase title="Proyectos" />
+      <Showcase title="Blog" />
       <Jumbotron />
     </main>
   );
