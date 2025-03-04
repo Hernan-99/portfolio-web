f9eabdb978c58fd7c7601b8a145ab3970efdaa21
Hernan-99 <hernan24744@gmail.com>
2025-03-04 14:34:57 -0300

    Evento onclick para el copiado del email

--- src/components/buttons/DefaultButton.jsx
+++ src/components/buttons/DefaultButton.jsx
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import ThemeContext from "../../context/ThemeContext";
 import styles from "./Buttons.module.css";
 
-export const DefaultButton = () => {
+export const DefaultButton = ({ onClick }) => {
   const { theme } = useContext(ThemeContext);
   return (
     <button
+      onClick={onClick}
       className={`${styles.contactme} ${
         theme === "dark" ? styles.contactmeDark : styles.contactmeLight
       }`}
