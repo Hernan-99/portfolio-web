14c22f99b3f7bfe18c5384c2db36836a71afc994
Hernan-99 <hernan24744@gmail.com>
2025-03-04 14:38:02 -0300

    Renderizado condicional para el copiado de email y pasaje de props

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ImgProfile from "../assets/img/ImgProfile.webp";
 import { Badge } from "./Badge";
 import { OutlineButton } from "./buttons/OutlineButton";
 import { ProfileImg } from "./ProfileImg";
 import ThemeContext from "../context/ThemeContext";
 import { DefaultButton } from "./buttons/DefaultButton";
+import { DocumentDuplicateIcon } from "@heroicons/react/16/solid";
+
 export const Hero = () => {
   const { theme } = useContext(ThemeContext);
+  const [copy, setCopy] = useState(false);
+  const handleCopyEmail = () => {
+    // navigator.clipboard.writeText("hernan24744@gmail.com"); // la api se bloquea en http por eso no puedo probarlo en el celular
+    setCopy(true);
+    setTimeout(() => {
+      setCopy(false);
+    }, 800);
+  };
   return (
     <section
       className={`hero dark-mode ${
@@ -36,15 +46,29 @@ export const Hero = () => {
           alignItems: "center",
         }}
       >
-        {/* <button
-          className={`contactme ${
-            theme === "dark" ? "contactme-dark " : "contactme-light"
-          }`}
-        >
-          Contactame
-        </button> */}
         <DefaultButton />
-        <OutlineButton value="Copy email" />
+        <OutlineButton
+          value="Copy email"
+          onClick={handleCopyEmail}
+          icon={DocumentDuplicateIcon}
+        />
+        {copy && (
+          <p
+            style={{
+              color: "#fff",
+              position: "absolute",
+              top: "250px",
+              borderRadius: "6px",
+              fontWeight: "600",
+              backgroundColor: "rgba(255,255,255,0.5)",
+              width: "80%",
+              padding: "20px 8px",
+              textAlign: "center",
+            }}
+          >
+            Copiado
+          </p>
+        )}
       </div>
     </section>
   );
