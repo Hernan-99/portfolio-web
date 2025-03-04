07a7723f7614e61423e2f46ae194f9b5db97afd0
Hernan-99 <hernan24744@gmail.com>
2025-03-04 14:35:33 -0300

    Evento onclick para el copiado del email y para descargar el cv

--- src/components/buttons/OutlineButton.jsx
+++ src/components/buttons/OutlineButton.jsx
@@ -2,14 +2,17 @@ import { useContext } from "react";
 import ThemeContext from "../../context/ThemeContext";
 import styles from "./Buttons.module.css";
 
-export const OutlineButton = ({ value }) => {
+export const OutlineButton = ({ value, onClick, icon: Icon }) => {
   const { theme } = useContext(ThemeContext);
+
   return (
     <button
+      onClick={onClick}
       className={`${styles.copyEmail} ${styles.shadow} ${
         theme === "dark" ? styles.copyEmailDark : styles.copyEmailLight
       }`}
     >
+      {Icon && <Icon className={styles.icon} />}
       {value}
     </button>
   );
