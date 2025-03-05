627146b06bd886668cda9c7f1e7f311ee80e1d0d
Hernan <hernan24744@gmail.com>
2025-03-05 14:34:39 -0300

    props en showcase para renderizado dinamico segun el tipo de contenido

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,8 +5,14 @@ export const Main = () => {
   return (
     <main>
       <Hero />
-      <Showcase title="Proyectos" />
-      <Showcase title="Blog" />
+
+      {/* 
+      Le pasamos type a Showcase porque es el componente que decide qué tipo de datos vamos a mostrar.
+      le decimos a cada Showcase qué tipo de contenido debe renderizar.
+      */}
+      <Showcase title="Proyectos" type="projects" />
+      <Showcase title="Diseño UX/UI" type="designs" />
+      <Showcase title="Blog" type="blogs" />
       <Jumbotron />
     </main>
   );
