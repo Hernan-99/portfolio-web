5b79c52d55dbbfbd49c6381c6c0741c95f9b3a73
Hernan-99 <hernan24744@gmail.com>
2025-03-05 23:01:04 -0300

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const { theme, handleTheme } = useContext(ThemeContext);
   return (
     <header
-      className={`dark-mode ${theme === "dark" ? "dark-mode" : "light-mode"}`}
+      className={`dark-mode ${theme === "dark" ? "dark-mode" : "light-mode-header"}`}
     >
       <Navbar theme={theme} handleTheme={handleTheme} />
     </header>
